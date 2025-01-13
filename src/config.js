bf4cd44c56b700a5e7b46b5869b785742347698d
energistix <50068022+energistix@users.noreply.github.com>
2025-01-13 18:32:37 +0100

    initial commit

---
+++ src/config.js
@@ -0,0 +1,23 @@
+import Phaser from "phaser";
+
+/**
+ * @type {Phaser.Types.Core.GameConfig}
+ */
+export default {
+  type: Phaser.AUTO,
+  width: 800, // TODO: decide on a good size
+  height: 600,
+  scale: {
+    mode: Phaser.Scale.NONE,
+    autoCenter: Phaser.Scale.Center,
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      // TODO: disable debug when not in dev mode
+      debug: true,
+      debugShowBody: true,
+    },
+  },
+  backgroundColor: "#87CEEB", // TODO: decide on a good color
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./style.css";
+import Phaser from "phaser";
+import config from "./config.js";
+// eslint-disable-next-line no-new -- we are not doing anything with the game instance yet
+new Phaser.Game(config);
