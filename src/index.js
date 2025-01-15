d74b2c7c6aa4963113586d03acf9cfe83851e1d2
Florian Chasseloup <florian.chasseloup@etudiant.univ-reims.fr>
2025-01-15 09:24:17 +0100

    Roughly started the unit class

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import "./style.css";
 import Phaser from "phaser";
 import config from "./config.js";
 import MenuScene from "./scenes/menu.js";
-import MainGameScene from "./scenes/maingame.js";
+import GameScene from "./scenes/game.js";
 
 const game = new Phaser.Game(config);
 game.scene.add("menu", new MenuScene(), false);
-game.scene.add("mainGame", new MainGameScene(), true);
+game.scene.add("game", new GameScene(), true);
