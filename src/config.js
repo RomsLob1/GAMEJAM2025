a8087908c189a3798a4e1623f0a4d132c3362767
davi0063 <clement.david1@etudiant.univ-reims.fr>
2025-01-16 02:29:10 +0100

    game windows size

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ import Phaser from "phaser";
 export default {
   type: Phaser.AUTO,
   width: 800, // TODO: decide on a good size
-  height: 600,
+  height: 700,
   scale: {
     mode: Phaser.Scale.NONE,
     autoCenter: Phaser.Scale.Center,
